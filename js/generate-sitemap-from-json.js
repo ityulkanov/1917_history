7fe3b643f58a55721c63584a5c1f64818246eb44
Igor Tiulkanov <ityulkanov@gmail.com>
2025-04-30 09:27:08 +0300

    sitemap

--- js/generate-sitemap-from-json.js
+++ js/generate-sitemap-from-json.js
@@ -7,8 +7,7 @@ const data = require('../data/posts.json'); // Replace with actual path if neede
 const baseUrl = 'https://ityulkanov.github.io/1917_history'; // Change to your actual domain
 const outputPath = path.join(__dirname, '../sitemap.xml');
 
-let sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n`;
-sitemap += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
+sitemap = `<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n`;
 
 data.forEach(post => {
   sitemap += `  <url>\n`;
